69a9e499256b00bf507f8a41919f1005a85e8f37
Abdul-Rahman Abdurrohman <abdulbarakeh98@gmail.com>
2021-10-07 23:13:14 +0200

    add os specific statements

--- src/testStep.js
+++ src/testStep.js
@@ -1,4 +1,12 @@
-const Constants = require('./constantsMac'); // WIN
+var os = require("os")
+var Constants = "";
+if(os.platform == 'win32'){
+    console.log("windows")
+    Constants = require('./constantsWin'); // WIN
+}else if(os.platform == 'darwin'){
+    console.log("mac")
+    Constants = require('./constantsMac'); // WIN
+}
 
 class TestSteps{
 
